8f0f126e214c03c484e9f190a2c482ad42fa3309
Johan Medina <josmedinaca@unal.edu.co>
2020-04-19 19:50:28 -0500

    update puertos

--- src/main/java/com/u_learn/ULearnApplication.java
+++ src/main/java/com/u_learn/ULearnApplication.java
@@ -2,7 +2,6 @@ package com.u_learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ULearnApplication {
 
