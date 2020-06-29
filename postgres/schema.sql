ed84c50ee21f8154f4dfb7d8616c7b150216bef5
Johan Medina <josmedinaca@unal.edu.co>
2020-06-29 18:14:36 -0500

    cambios en dockerfile

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -1,12 +1,11 @@
-
-CREATE TABLE IF NOT EXISTS "u_learn"."public"."curso"(
-    "id_curso" SERIAL       NOT NULL,
-    "nombre" character varying(500) NOT NULL,
-    "categoria" character varying(100) NOT NULL,
-    "duracion" character varying(100) NOT NULL,
-    "nombre_profesor" character varying(100) NOT NULL,
-    "calificacion" character varying(100),
-	PRIMARY KEY ("id_curso")
+CREATE TABLE IF NOT EXISTS curso(
+    id_curso SERIAL       NOT NULL,
+    nombre character varying(500) NOT NULL,
+    categoria character varying(100) NOT NULL,
+    duracion character varying(100) NOT NULL,
+    nombre_profesor character varying(100) NOT NULL,
+    calificacion character varying(100),
+	PRIMARY KEY (id_curso)
 );
 
 
